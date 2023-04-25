15c473d18e13ff88c3b0effdbf582c4f0a0bf824
Prem Karira <karira46@gmail.com>
2023-04-26 04:22:40 +0530

    initial

---
+++ main.py
@@ -0,0 +1,67 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from motor.motor_asyncio import AsyncIOMotorClient
+from bson.objectid import ObjectId
+from dotenv import load_dotenv
+
+load_dotenv()
+import os
+app = FastAPI()
+
+# Connect to MongoDB
+url=os.environ.get('MONGO_URL')
+print(os.environ.get('MONGO_URL'))
+client = AsyncIOMotorClient(url)
+db = client["task_management"]
+tasks_collection = db["tasks"]
+
+# Task model
+class Task(BaseModel):
+    id: str = None
+    title: str
+    description: str
+    assignee: str = None
+    priority: int = 1
+    due_date: str = None
+
+# Create a task
+@app.post("/tasks")
+async def create_task(task: Task):
+    task_data = task.dict()
+    task_data["_id"] = ObjectId()
+    await tasks_collection.insert_one(task_data)
+    return {"id": str(task_data["_id"])}
+
+# Get all tasks
+@app.get("/tasks")
+async def get_all_tasks():
+    
+    tasks = []
+    async for task in tasks_collection.find():
+        task["id"] = str(task["_id"])
+        del task["_id"]
+        tasks.append(task)
+    return tasks
+
+# Get a task by ID
+@app.get("/tasks/{task_id}")
+async def get_task(task_id: str):
+    task = await tasks_collection.find_one({"_id": ObjectId(task_id)})
+    if task:
+        task["id"] = str(task["_id"])
+        del task["_id"]
+        return task
+    else:
+        return {"error": "Task not found"}
+
+# Update a task
+@app.put("/tasks/{task_id}")
+async def update_task(task_id: str, updated_task: Task):
+    await tasks_collection.update_one({"_id": ObjectId(task_id)}, {"$set": updated_task.dict()})
+    return {"status": "Task updated"}
+
+# Delete a task
+@app.delete("/tasks/{task_id}")
+async def delete_task(task_id: str):
+    await tasks_collection.delete_one({"_id": ObjectId(task_id)})
+    return {"status": "Task deleted"}
